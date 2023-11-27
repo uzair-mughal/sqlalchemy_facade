217e273de824e18326e7c532deda1f74b31427f1
Uzair <uzam.dev@gmail.com>
2023-11-27 20:23:33 +0500

    updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name="sqlalchemy_facade",
     packages=find_packages(),
-    version="2.0.23",
+    version="2.0.0",
     license="MIT",
     description="",
     author="Uzair Ahmed Mughal",
